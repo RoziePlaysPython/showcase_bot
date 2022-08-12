a8d60c64568c89f6e9e5e55e2ff15f85f861e632
RoziePlaysPython <tfatugava@gmail.com>
2022-08-12 12:28:27 +0300

    CSVDumpster object is now passed to disp.bot_data dict

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #/usr/bin/python3
 from dataclasses import dataclass
 from sys import argv
+from setuptools import Command
 from telegram import Bot
 from telegram.ext import CommandHandler
 from telegram.ext import CallbackQueryHandler as CQHandler
@@ -14,9 +15,10 @@ from dumpsters import CSVDumpster
 
 
 BOT_TOKEN = argv[1]
-upd = Updater(token=BOT_TOKEN, use_context=True)
-upd.bot.data['dumpster'] = CSVDumpster(filename = filebase.HIREME_FILENAME)
+bot = Bot(token=BOT_TOKEN)
+upd = Updater(bot=bot, use_context=True)
 disp = upd.dispatcher
+disp.bot_data['dumpster'] = CSVDumpster(filename = filebase.HIREME_FILENAME)
 logging.basicConfig(format='>%(asctime)s - %(name)s - %(levelname)s - %(message)s<', level=logging.INFO)
 
 # function zone
@@ -30,22 +32,26 @@ def help_message(update, context):
 
 def hireme_message(update, context):
     context.bot.send_message(chat_id = update.effective_chat.id, text = messages.hireme_message)
-    userstats = [
+    userstats = (
             update.effective_user.id,
             update.effective_user.username,
             update.effective_user.language_code
-            ]
-    context.bot.data['dumpster'].writedata(userstats)
+    )
+    context.bot_data['dumpster'].writedata(userstats)
     return
 
             
 
 #handler zone
 start_handler = CommandHandler('start', start)
+help_handler = CommandHandler('help', help_message)
+hireme_handler = CommandHandler('hireme', hireme_message)
 
 #disp.add_handler zone
 disp.add_handler(start_handler)
+disp.add_handler(help_handler)
+disp.add_handler(hireme_handler)
+
 
 #start polling
 upd.start_polling()
-
